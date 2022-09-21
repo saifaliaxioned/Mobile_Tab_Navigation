12cbc1c6306b3894a807d7b4c6b385cf0a89828e
saif <saifalis@axioned.com>
2022-09-21 18:47:17 +0530

    tab navigation function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
-/* Author: 
+// Tab Navigation function
+var imageList = document.querySelectorAll('.image-list');
+var navList = document.querySelectorAll('.nav-list');
 
-*/
+
+navList.forEach(function Iterate(navLink , imageIndex) {
+  navLink.addEventListener('click',function ShowImage() {
+    var activeImage = document.querySelector('.active-list');
+    var activeLink = document.querySelector('.nav-list-active');
+    activeLink.classList.remove('nav-list-active');
+    activeImage.classList.remove('active-list');  
+    navLink.classList.add('nav-list-active');
+    imageList[imageIndex].classList.add('active-list');
+  })
+})
 
 
 
